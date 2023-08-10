96862b3a6fd6d6604d9f0d117a087324848c54fc
gnauheinna <116969903+gnauheinna@users.noreply.github.com>
2023-08-10 21:25:30 +0800

    finished webapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+import * as tf from "@tensorflow/tfjs";
+import * as qna from "@tensorflow-models/qna";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
+import { Fragment } from "react";
+
+const App = () => {
+  // references and state hooks
+  const passageRef = useRef(null);
+  const questionRef = useRef(null);
+  const [answer, setAnswer] = useState();
+  const [model, setModel] = useState(null);
+
+  // Load Tensorflow Model
+  const loadModel = async () => {
+    const loadedModel = await qna.load();
+    setModel(loadedModel);
+    console.log("Model loaded.");
+  };
+
+  // Questions Handleer
+  const answerQuestion = async (e) => {
+    if (e.which === 13 && model !== null) {
+      console.log("Question submitted.");
+      const passage = passageRef.current.value;
+      const question = questionRef.current.value;
+
+      const answers = await model.findAnswers(question, passage);
+      setAnswer(answers);
+      console.log(answers);
+    }
+  };
+
+  useEffect(() => {
+    loadModel();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {model == null ? ( // checks if model loaded
+          <div>
+            <div>Model Loading</div>
+            <Loader type="Puff" color="#00BFFF" height={100} width={100} />
+          </div>
+        ) : (
+          // If our model has loaded
+          <Fragment>
+            Passage
+            <textarea ref={passageRef} rows="30" cols="100"></textarea>
+            Ask a Question
+            <input
+              ref={questionRef}
+              onKeyDown={answerQuestion}
+              size="80"
+            ></input>
+            Answers
+            {answer // Ternary check
+              ? answer.map((ans, idx) => (
+                  <div key={idx}>
+                    <b>Answer {idx + 1} -</b>
+                    {ans.text} ({Math.floor(ans.score * 100) / 100})
+                  </div>
+                ))
+              : ""}
+          </Fragment>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
